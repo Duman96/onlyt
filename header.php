45e1c257f7b31b19edbdebb81e048dd34f674232
Duman96 <dukaiklasov96@gmail.com>
2021-01-24 18:22:19 +0600

    CSS 1

--- header.php
+++ header.php
@@ -3,16 +3,46 @@
   <head>
     <meta charset="utf-8">
     <title>Only-Tech</title>
+    <link rel="icon" href="/onlyt/assets/img/ot.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/onlyt/css/signin.css">
   </head>
   <body>
 
-    <nav>
-      <div class="wrapper">
-        <ul>
-          <li> <a href="index.php">Home</a> </li>
-          <li> <a href="login.php">Login</a> </li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="/onlyt/assets/img/ot.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        Only-Tech
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
         </ul>
 
+        <form class="form-inline mx-3 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+        <a class="nav-link" href="login.php">Sign In<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Sign Up<span class="sr-only">(current)</span></a>
+
       </div>
-      
     </nav>
--- login.php
+++ login.php
@@ -2,16 +2,23 @@
   include_once 'header.php'
  ?>
 
-  <section>
-    <h2>Log In</h2>
-    <div class="">
-      <form class="" action="login.inc.php" method="post">
-        <input type="text" name="name" placeholder="Login...">
-        <input type="password" name="pwd" placeholder="Password...">
-        <button type="submit" name="submit">Log In</button>
+  <section class="text-center">
+      <form class="form-signin" action="login.inc.php" method="post">
+        <img class="mb-4" src="/onlyt/assets/img/ot.png" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="inputEmail" class="sr-only">Login</label>
+        <input type="text" name="name" id="inputEmail" class="form-control" placeholder="Login..." required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="pwd" id="inputPassword" class="form-control" placeholder="Password..." required>
+        <div class="checkbox mb-3">
+          <label>
+            <input type="checkbox" name="" value="remember-me"> Remember Me
+          </label>
+
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Sign In</button>
       </form>
 
-    </div>
   </section>
 
 
